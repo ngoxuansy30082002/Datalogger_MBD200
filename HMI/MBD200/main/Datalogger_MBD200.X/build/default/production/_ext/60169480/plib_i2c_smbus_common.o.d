d61b1e9f012980d769a30e471748c76b031071e1
Tuspark-Nova <heavystorm2708@gmail.com>
2026-03-20 15:45:13 +0700

    Delete HMI/MBD200/main directory

--- HMI/MBD200/main/Datalogger_MBD200.X/build/default/production/_ext/60169480/plib_i2c_smbus_common.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/_ext/60169480/plib_i2c_smbus_common.o: \
- ../src/config/default/peripheral/i2c/plib_i2c_smbus_common.c \
- ../src/config/default/peripheral/i2c/plib_i2c_smbus_common.h
-
-../src/config/default/peripheral/i2c/plib_i2c_smbus_common.h:
